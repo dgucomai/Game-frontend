9950dad220dcf18ffd1c5854605475b169d683f0
bms0699 <bmsbms100100@gmail.com>
2026-05-11 19:39:55 +0900

    플래피버드 기둥구현

--- Assets/Scripts/1.FlappyBird/Score.cs
+++ Assets/Scripts/1.FlappyBird/Score.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 using UnityEditor.UI;
 using UnityEngine.UI;
+using TMPro;
+
 public class Score : MonoBehaviour
 {
-    public  static int score = 0;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static int score = 0;
+
+    TMP_Text text;
+
     void Start()
     {
-        
+        text = GetComponent<TMP_Text>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        GetComponent<Text>().text = score.ToString();
+        text.text = score.ToString();
     }
 }
